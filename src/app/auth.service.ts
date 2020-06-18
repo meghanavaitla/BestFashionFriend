75e7e70021670eedfdcc139f0a24c0406f442557
meghanavaitla <48142167+meghanavaitla@users.noreply.github.com>
2020-06-18 20:08:48 +0530

    path changed

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import  {environment} from '../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
-  path=environment.path;
+  path= 'http://localhost:8000';
   TOKEN_KEY='token'
   constructor(private http:HttpClient) { }
   get token()
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  path: 'http://localhost:8000'
+  production: false
 };
 
 /*

412ac9b0d6f5c763349af57db2450d184275d2a8
meghanavaitla <48142167+meghanavaitla@users.noreply.github.com>
2020-06-19 01:17:25 +0530

    last commit for profile

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcrypt=require('bcrypt')
+var bcrypt=require('bcryptjs')
 var numSaltRounds = 10;
 var userSchema = new mongoose.Schema({
     firstname: String,

0f682d1f687ff47429024ae207cf57f9c20d48f4
meghanavaitla <48142167+meghanavaitla@users.noreply.github.com>
2020-06-18 22:55:55 +0530

    working properly until profile

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors =require('cors');
+var jwt = require('jwt-simple')
+var bcrypt = require('bcrypt')
+var User = require('./models/user');
 var blouse = require('./models/blouses');
 var bottoms = require('./models/bottoms');
 var womenaccessories = require('./models/womenaccessories');
@@ -12,6 +15,7 @@ var jackets = require('./models/jackets');
 var menaccessories = require('./models/menaccessories');
 var shorts = require('./models/shorts');
 
+mongoose.Promise = Promise;
 // ************************ DB Connection ************************
 
 var dbOptions = {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, auto_reconnect: true};
@@ -55,6 +59,7 @@ app.use(cors());
 
 
 // ****** Body Parser *******
+
 app.get('/blouse', (req, res) => {
     blouse.find({},null, {
       limit: 20
@@ -188,6 +193,71 @@ app.get('/blouse', (req, res) => {
     });
   });
 
+  app.get('/users', async (req, res) => {
+    try {
+
+        var users = await User.find({}, '-password -__v')
+        res.send(users)
+    }
+    catch (error) {
+        console.error(error)
+        res.sendStatus(500)
+    }
+
+});
+
+app.get('/profile/:id', async (req, res) => {
+
+  try {
+      var user = await User.findById(req.params.id, '-password -__v')
+      res.send(user)
+  }
+  catch (error) {
+      console.error(error)
+      res.sendStatus(500)
+  }
+
+});
+app.post('/register', (req, res) => {
+   
+  var userData = req.body;
+  var user = new User((userData));
+  user.save((err, newuser) => {
+      if (err)
+      res.json({success:false, msg: 'failed to register user'});
+
+      var payload = { sub: newuser._id }
+
+      var token = jwt.encode(payload, '123')
+
+      res.status(200).send({ token })
+      res.json({success:true, msg: 'user registered'});
+
+  });
+});
+
+app.post('/login', async (req, res) => {
+  var loginData = req.body;
+
+  var user = await User.findOne({ email: loginData.email })
+
+  if (!user)
+      return res.status(401).send({ message: 'Email or password Invalid' })
+
+  bcrypt.compare(loginData.password, user.password, (err, isMatch) => {
+      if (!isMatch)
+          return res.status(401).send({ message: 'Email or password Invalid' })
+      var payload = { sub: user._id }
+
+      var token = jwt.encode(payload, '123')
+
+      res.status(200).send({ token })
+  })
+
+  // if (loginData.password != user.password)
+  //     return res.status(401).send({ message: 'Email or password Invalid' })
+
+});
 
 // ****** Body Parser ********
 
